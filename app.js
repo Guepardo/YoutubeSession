393cda0739a84236ad40534356f79a302238ccc4
Guepardo <bsinet@hotmail.com>
2015-12-25 14:11:31 -0200

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require("express"); 
+var path    = require("path"); 
+var session = require("client-sessions"); 
+var http    = require("http"); 
+
+var app = express(); 
+
+//Arquivo de configuração de rotas: 
+var routes = require("./routes/routes"); 
+app.use('/', routes ); 
+
+//Registrando pasta de arquivos estáticos: 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Registrando view engine: 
+app.set('views', __dirname + '/views'); 
+app.engine('html', require('ejs').renderFile); 
+app.set('view engine', 'ejs'); 
+
+//Iniciando servidor: 
+var port = process.env.PORT || 3000; 
+
+app.set('port', port ); 
+var server = http.createServer(app);
+server.listen(port);  
