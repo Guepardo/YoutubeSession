6ee92ddaff7e878fe70b251706cdbfc1146e22f4
Guepardo <bsinet@hotmail.com>
2015-12-27 05:09:26 -0200

    sync

---
+++ modules/Room.js
@@ -0,0 +1,39 @@
+
+var Room = function(){}; 
+
+Room.prototype.videoDuration = function(){
+	return this.video_duration; 
+}; 
+
+Room.prototype.setVideoInformation = function(video_name,video_duration){
+	this.video_name = video_name; 
+	this.video_duration = video_duration; 
+}; 
+
+Room.prototype.registerSessionName = function(session_name){
+	this.session_name = session_name; 
+}; 
+
+Room.prototype.registerOwner = function(hash){
+	if(this.room_owner != 'undefined' ) return; 
+	
+	this.room_owner = hash; 
+}; 
+
+Room.prototype.startSession = function(hash){
+	this.timeout_session = this.video_duration; //provavelmente vou ter que fazer conversão para segundos aqui; 
+
+	if( hash !== this.room_owner )
+		return false;
+
+	return true; 
+}
+
+Room.prototype.stopSession = function(){
+	if( hash !== this.room_owner )
+		return false;
+	//some code here...
+}; 
+
+
+module.exports = Room; 
--- modules/serverSocket.js
+++ modules/serverSocket.js
@@ -1,39 +1,51 @@
-var Server = require('socket.io'); 
+var Server = require('socket.io');
+var crypto = require('crypto');  
+var Room   = require('./Room'); 
 
 var socketServer ={
 	io : null, 
 	rooms: [], 
 
 	init : function(server){
-		io = new Server (server); 
-		console.log('iniciado'); 
+		this.io = io = new Server (server); 
+
+		console.log('!Servidor socket iniciado.'); 
 
 		io.on('connection', function(socket){
 
 			socket.on('enterRoom',function(data){
+				var hash = crypto.createHash('md5'); 
 				console.log("usuário registrando sala"); 
-				socket.room = data.room; 
+
+				socket.__room = data.room; 
+				socket.hashName = hash.digest( new Date().toString()); 
 				socket.join(data.room); 
 
-				io.sockets.in(socket.room).emit('newUser',{data : 'Um usuário acabou de entrar'}); 
-
+				io.sockets.in(socket.__room).emit('newUser',{data : 'Um usuário acabou de entrar'}); 
 			}); 
 
 			socket.on('msg',function(data){
-				console.log("mensagem em broadcast para a sala "+ socket.room);
-				io.sockets.in(socket.room).emit('msg',data); 
-				//console.log(io.sockets); 
+				console.log("mensagem em broadcast para a sala "+ socket.__room);
+				io.sockets.in(socket.__room).emit('msg',data); 
 			}); 
 
 			socket.on('disconnect', function(){
-				io.sockets.in(socket.room).emit('leave',{ data: 'Usuários deixou a sala '+ socket.room}); 
-			}); 
+				io.sockets.in(socket.__room).emit('leave',{ data: 'Usuários deixou a sala '+ socket.__room}); 
+			});
+
+			socket.on('registerOwner', function(){
+				if(socket.__room == 'undefined') return; 
+				rooms[socket.__room].registerOwner(socket.__hashName); 
+			})
 
 		}); 	
 	}, 
 
 	createRoom: function(hashName){
-		this.rooms[hashName] = hashName;  
+		this.rooms[hashName] = new Room();  
+		this.rooms[hashName].registerSessionName(hashName); 
+		console.log("Informações sobre a Room"); 
+		console.log(this.rooms[hashName]); 
 	},
 
 	deleteRoom: function(hashName){
