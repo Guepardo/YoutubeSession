f2f17de8c2731c956a0ed3ee4f4e085866b16cad
Guepardo <bsinet@hotmail.com>
2015-12-29 12:13:29 -0200

    Criando a classe WindowSession e acoplando o iframe do youtube na página.

---
+++ public/js/session_app.js
@@ -0,0 +1,77 @@
+
+// Start App
+var player;
+function onYouTubeIframeAPIReady() {
+   player = new YT.Player('player', {
+       height: '390',
+       width: '640',
+       videoId: 'I0YTBj0WHkU', 
+       playerVars: {
+         'autoplay': 1, 
+          'controls':  0
+       }
+    });
+}
+
+//App lib
+var WindowSession = function(){
+   const FRENQUENCY_INTERVAL = 100; 
+  // creating interval to check changes on youtube player: 
+  var intervalCheck = setInterval(this.checkPlayerChange,FRENQUENCY_INTERVAL); 
+}; 
+
+WindowSession.prototype.checkPlayerChange = function(){
+
+}
+
+WindowSession.prototype.newAvatar = function(){
+
+}; 
+
+WindowSession.prototype.removeAvatar = function(){
+	
+}; 
+
+WindowSession.prototype.avatarStatusChange = function(){
+	
+}; 
+
+WindowSession.prototype.newMessage = function(){
+	
+}; 
+
+WindowSession.prototype.playerStop = function(){
+	
+}; 
+
+WindowSession.prototype.playerPause = function(){
+	
+}; 
+
+WindowSession.prototype.playerSeek = function(){
+	
+}; 
+
+WindowSession.prototype.typingChange = function(){
+	
+};
+
+WindowSession.prototype.onPlay = function(){
+	
+}; 
+
+WindowSession.prototype.onPause = function(){
+	
+}; 
+
+WindowSession.prototype.onSeek = function(){
+	
+}; 
+
+WindowSession.prototype.onStop = function(){
+	
+}; 
+
+WindowSession.prototype.onBuffering = function(){
+	
+}; 
