b4481a4087ca5ae6c2f5149a4dce069ca3d59b5a
Guepardo <bsinet@hotmail.com>
2016-01-06 09:16:02 -0200

    Adicionando sockets, splashscream e redirecionamento da tela index para a tela de sessão

--- public/js/session_app.js
+++ public/js/session_app.js
@@ -5,7 +5,7 @@ function onYouTubeIframeAPIReady() {
    player = new YT.Player('player', {
        height: '390',
        width: '640',
-       videoId: 'I0YTBj0WHkU', 
+       videoId: '', 
        playerVars: {
          'autoplay': 1, 
           'controls':  0
